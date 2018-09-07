61969cf3327c7babf65001ba02fed2e7e9e7e227
Joshua David Hayes <hayesjosh@gmail.com>
2018-09-07 13:42:24 -0700

    hm

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def about():
   return render_template('about.html')
 
 if __name__ == '__main__':
-  port = int(os.environ.get("PORT", 5000))
-  app_ticker.run(host='0.0.0.0', port=port)
+  #port = int(os.environ.get("PORT", 5000))
+  app_ticker.run(host='0.0.0.0', port=33507)
   #app_ticker.run(port=33507)
   #app_ticker.run(debug=True)
